3e569ebe7278646020edb6b18c962fb042eb073e
Thhethssmuz <thhethssmuz@gmail.com>
2020-01-24 19:58:19 +0100

    increase test wait duration to limit potential timing issues in test

--- test/spawn/await.js
+++ test/spawn/await.js
@@ -5,13 +5,13 @@ const wait = duration => new Promise(resolve => setTimeout(resolve, duration));
 
 test('await', async function () {
   this.test('subtest', async function () {
-    await wait(100);
+    await wait(1000);
   });
   this.notThrows(async function () {
-    await wait(100);
+    await wait(1000);
   });
   this.throws(async function () {
-    await wait(100);
+    await wait(1000);
     throw new Error('lol');
   });
 });
